8188dc5f14865415f8110b5ab6bd94f385557034
ST-byte441 <seungtaekoh5@gmail.com>
2025-06-26 15:38:13 -0700

    Feat: Homepage buffering + styling, Secondpage backend functionality

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -5,47 +5,59 @@ import { useNavigate, Link } from 'react-router-dom';
 export function HomePage() {
     const navigate = useNavigate();
     const [userVibe, setUserVibe] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     async function handleSubmit (event) {
         event.preventDefault();
+        setIsLoading(true);
 
-        // try {
-        //     const response = await fetch ('/search', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         body: JSON.stringify(userVibe)
-        //     })
+        try {
+            console.log('making fetch request to /search');
 
-        //     if (response.ok) {
-        //         sessionStorage.setItem('userVibe', userVibe)
-        //         navigate ('/playlist')
-        //     } else {
-        //         alert ('Unknown Error: Try Refreshing the page!')
-        //     }  
-        // } catch (err) {
-        //     console.log('UserSubmit Error: ' +  err);
-        //     alert ('Unknown Error: Try Refreshing the page!')
-        // }
+            const response = await fetch ('/search', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({userVibe: userVibe})
+            })
 
-        sessionStorage.setItem('userVibe', userVibe) //! Delete after backend is connected
-        navigate ('/playlist') //! Delete after backend is connected
+            console.log('Response status: ', response.status)
+
+            if (response.ok) {
+                const playlistData = await response.json();
+                sessionStorage.setItem('userVibe', userVibe)
+                sessionStorage.setItem('currentPlaylist', JSON.stringify(playlistData));
+                navigate ('/playlist')
+            } else {
+                alert ('Response Error: Try Refreshing the page!')
+            }  
+        } catch (err) {
+            console.log('UserSubmit Error: ' +  err);
+            alert ('Unknown Error: Try Refreshing the page!')
+        } finally {
+            setIsLoading(false);
+        }
+
     }
 
     return (
         <div id="homePage">
-             <header id="mainHeader">
-            
-              </header>
+             <div className="logo-container">
+                <img src="./src/assets/vibelist_logo.png" alt="Logo" className="logo" />
+             </div>
             <form id ="formStyle" onSubmit={handleSubmit}>
-              <h1 id="promptQuestion">What’s Your Vibe? We’ve Got the Music!</h1>
+              <h1 id="promptQuestion">Your Vibes. Our playlist</h1>
               <input 
                 id="inputStyle"
+                className={isLoading ? 'loading' : ''}
                 type="text" 
                 // placeholder='Insert your current vibe' 
                 value={userVibe} 
-                onChange={(e) => setUserVibe(e.target.value)}/>
+                onChange={(e) => setUserVibe(e.target.value)}
+                disabled={isLoading} // prevent multiple requests to the backend
+                />
+              <button type="submit" style={{display : 'none'}}>Submit</button>
             </form>
         </div>
     )
