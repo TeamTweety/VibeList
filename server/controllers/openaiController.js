d57a69e8b3de8671be877db358ade1794d01356b
Parker Redford <parkermasaru@gmail.com>
2025-06-25 22:18:17 -0400

    feat(openai): Successful JSON parse sent

--- server/controllers/openaiController.js
+++ server/controllers/openaiController.js
@@ -8,6 +8,21 @@ const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 export const queryOpenAI = async (req, res, next) => {
   try {
     const { userVibe } = res.locals;
+    const { currentPlaylist } = res.locals; // Are these what we are calling them?
+    const { rejectedSongs } = res.locals; //?
+    const { numOfSongs } = res.locals; //?
+
+
+    const userPrompt = `Your job is to recommend songs that match the mood, tone, and energy of the vibe. The current vibe is ${userVibe}.`
+    const example = `[{ "track": "track name", "artist": "artist name" }, { "track": "track name", "artist": "artist name" }]` // do we need more properties?
+    const systemPrompt = ` This is the "Current Playlist": ${currentPlaylist}. 
+                    This is the "Rejected Songs": ${rejectedSongs}.
+                    If you are not provided a Current Playlist OR Rejected Songs, you must recommend exactly 30 song.
+                    If you ARE provided "Current Playlist" and "Rejected Songs", you must recommend exaclty ${numOfSongs} that are not found in "Current Playlist" or "Rejected Songs".
+                    The format of the response should be exactly like this example: ${example}. Do not respond with anything else.`
+
+    
+                    
 
     if (!userVibe) {
       const error = {
@@ -22,22 +37,19 @@ export const queryOpenAI = async (req, res, next) => {
       input: [
         {
           role: 'user',
-          content: `This is the query given by the user: ${userVibe}. Suggest 30 songs that match the style, energy, and mood.`,
+          content: userPrompt,
         },
         {
-          role: 'user',
-          content: '',
+          role: 'system',
+          content: systemPrompt,
         }
       ],
     });
 
-    console.log('this is the completion ', completion)
-    console.log('this is the completion output text', completion.output_text)
-
     const queryResult = completion.output_text;
-    res.locals.userVibeQuery = queryResult;
+    res.locals.userVibeQuery = JSON.parse(queryResult);
 
-    console.log('THIS IS THE UserVibeQuery', queryResult)
+    console.log('THIS IS THE UserVibeQuery', JSON.parse(queryResult));
 
     return next();
   } catch(err) {
