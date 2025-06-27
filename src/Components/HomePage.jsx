1f8aa525b6e3a9b102cd7b5603b7527beca0fb7d
ST-byte441 <seungtaekoh5@gmail.com>
2025-06-27 08:43:58 -0700

    fix: emergency fix for homepage break

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { Test } from './testComponent';
 
 export function HomePage() {
     const navigate = useNavigate();
